1c1b5c05b9ff3edd33632f03f5555c95ca78e614
Sho Hashimoto <hashimoto@shokai.org>
2011-02-07 23:12:15 +0900

    css

--- assets/www/main.js
+++ assets/www/main.js
@@ -2,21 +2,23 @@ var result = '';
 var g = new GlitchText();
 
 $(function(){
-    $('input#glitch').click(glitch);
-    $('input#tweet').click(tweet);
+    $('div#glitch').click(glitch);
+    $('div#tweet').click(tweet).css('visibility', 'hidden');
 });
 
 
 var glitch = function(){
     var source = $('textarea#source').val();
+    if(source.length < 1) return;
     result = source;
     while(true){
         result = g.random(source);
         if(Math.random() > 0.4) break;
     }
     $('div#result').html(result);
+    $('div#tweet').css('visibility','visible');
 };
 
 var tweet = function(){
-    location.href = 'http://twitter.com/home?status='+result;
+    window.open('http://twitter.com/home?status='+result);
 };
